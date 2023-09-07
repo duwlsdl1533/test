8a708446ae733ee3d72b02ec8d8f190bbdc6528b
duwlsdl1533 <58278280+duwlsdl1533@users.noreply.github.com>
2023-09-07 13:02:43 +0900

    Add files via upload

---
+++ cve/cve/src/test/java/cve/cve/CveApplicationTests.java
@@ -0,0 +1,13 @@
+package cve.cve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
